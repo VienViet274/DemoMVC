a62e399741c0e6101608f4f7878b3afe6154e45a
VIỆT VÕ VIỄN <22880297@student.hcmus.edu.vn>
2023-10-09 17:39:03 +0700

    Update Product Img error

---
+++ DemoMVC/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using DemoMVC.Data;
+using DemoMVC.Models;
+using DemoMVC.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DemoMVC.Areas.Admin.Controllers
+{
+    [Area(nameof(Admin))]
+    [Authorize(Roles =SD.Role_User_Admin)]
+    public class UserController : Controller
+    {
+        private DataContext _context;
+        public UserController(DataContext context) {
+            _context= context;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<ApplicationUser> ds=_context.ApplicationUser.Include("CompanyKey").ToList();
+            
+           /* IEnumerable<ApplicationUser>ds= _context.ApplicationUser.ToList();
+            _context.Database.CloseConnection();
+            foreach(var a in ds)
+            {
+                a.CompanyKey = _context.Companies.Where(x => x.ID == a.CompanyID).FirstOrDefault();
+            }*/
+            
+            
+            return View(ds);
+        }
+    }
+}
